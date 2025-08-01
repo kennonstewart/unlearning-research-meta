665e86d2d632f36cf4fd9812d46c88470a7f1162
Kennon Stewart <kennon.stewart@gmail.com>
2025-08-01 21:29:11 +0000

    expanding the range of values for the linear data stream

--- code/data_loader/linear.py
+++ code/data_loader/linear.py
@@ -18,7 +18,7 @@ def get_synthetic_linear_stream(dim=20, seed=42, noise_std=0.1):
     set_global_seed(seed)
     rng = np.random.default_rng(seed)
     w_star = rng.normal(size=dim)
-    X = rng.normal(size=(100000, dim)).astype(np.float32)
-    noise = rng.normal(scale=noise_std, size=100000).astype(np.float32)
+    X = rng.normal(size=(1000000, dim)).astype(np.float32)
+    noise = rng.normal(scale=noise_std, size=1000000).astype(np.float32)
     y = X @ w_star + noise
     return make_stream(X, y, mode="iid", seed=seed)
